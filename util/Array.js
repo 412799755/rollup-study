0339a6582bd4f3dde81154db358b7b4db5aa8246
412799755 <412799755@qq.com>
2019-06-12 10:07:34 +0800

    fix IE

--- util/Array.js
+++ util/Array.js
@@ -36,6 +36,58 @@ function duplicates(arr) {
     }
     return rel;
 }
+//ie6不支持indexOf
+var indexOf = [].indexOf ?
+    function(arr, item) {
+        return arr.indexOf(item)
+    } :
+    function indexOf(arr, item) {
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i] === item) {
+                return i
+            }
+        }
+        return -1
+    }
+    //IE 不可枚举属性bug
+var obj = { toString: 1, hasOwnProperty: 1 }
+for (var p in obj) {
+    console.log(p)
+}
+var keys = Object.keys || (function () {
+    var hasOwnProperty = Object.prototype.hasOwnProperty,
+        hasDontEnumBug = !{toString:null}.propertyIsEnumerable("toString"),
+        DontEnums = [
+            'toString',
+            'toLocaleString',
+            'valueOf',
+            'hasOwnProperty',
+            'isPrototypeOf',
+            'propertyIsEnumerable',
+            'constructor'
+        ],
+        DontEnumsLength = DontEnums.length;
+
+    return function (o) {
+        if (typeof o != "object" && typeof o != "function" || o === null)
+            throw new TypeError("Object.keys called on a non-object");
+
+        var result = [];
+        for (var name in o) {
+            if (hasOwnProperty.call(o, name))
+                result.push(name);
+        }
+
+        if (hasDontEnumBug) {
+            for (var i = 0; i < DontEnumsLength; i++) {
+                if (hasOwnProperty.call(o, DontEnums[i]))
+                    result.push(DontEnums[i]);
+            }
+        }
+
+        return result;
+    };
+})();
 //es6
 function duplicates(arr) {
     return Array.from(new Set(arr));
