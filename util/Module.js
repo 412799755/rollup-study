60077b3fcb337fb85e54514b29c271abfd5de0ca
412799755 <412799755@qq.com>
2019-06-12 17:50:57 +0800

    fake requireJs

--- util/Module.js
+++ util/Module.js
@@ -18,3 +18,65 @@ require(['a', 'b'], function (a, b) {
 }, function () {
     console.error('Something wrong with the dependent modules.');
 });
+Module.prototype.analyzeDep = function () {
+    // .......
+    let depCount = this.dep ? this.dep.length : 0;
+    Object.defineProperty(this, 'depCount', {
+        get() {
+            return depCount;
+        },
+        set(newDepCount) {
+            depCount = newDepCount;
+            if (newDepCount === 0) {
+                console.log(`模块${this.name}的依赖已经全部准备好`);
+                this.execute();  // 如果depCount===0，执行该模块
+            }
+        }
+    });
+    this.depCount = depCount;
+    // ....
+};
+Object.defineProperty(this, 'status', {
+    get () {
+        return status;
+    },
+    set (newStatus) {
+        status = newStatus;
+        if (status === 5) {
+            // 假如某个模块已经准备好了（STATUS===5），
+            // 那么找出依赖于这个模块的所有模块，让他们都执行depCount--
+            let depedModules = mapDepToModule[this.name];
+            if (!depedModules) return;
+            depedModules.forEach((module) => {
+                setTimeout(() => {
+                    module.depCount--;
+                });
+            });
+        }
+    }
+})
+
+// before
+require = function (dep, cb, errorFn) {
+    // mainEntryModule是主入口模块
+    modules[mainEntryModule.name] = mainEntryModule;
+    mainEntryModule.dep = dep;
+    mainEntryModule.cb = cb;
+    mainEntryModule.errorFn = errorFn;
+    mainEntryModule.analyzeDep();
+};
+
+// after
+require = function (dep, cb, errorFn) {
+    let task = new Task(dep, cb, errorFn);
+    task.analyzeDep();
+};
+
+// 引入新的类: Task(任务)
+function Task(dep, cb, errorFn) {
+    this.tid = ++tid;
+    this.init(dep, cb, errorFn);
+}
+
+// Task类继承于Module类
+Task.prototype = Object.create(Module.prototype);
